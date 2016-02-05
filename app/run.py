899db4316ce43610b6a68323fa75500b04b85851
moliri <lmaliakal@u.northwestern.edu>
2016-02-05 12:29:01 -0600

    updating function calls with new arg

--- app/run.py
+++ app/run.py
@@ -20,39 +20,44 @@ def index():
 @app.route('/fakerun/')
 def fakeRun():
     global csvOfRun
+    global runnerUpdateQuery = fr.getRunnerUpdateQuerySet
     if csvOfRun == None:
         csvOfRun = open('./static/data/fakeRunnerInEv.csv', 'r').readlines()
-    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "delta5", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (runnerUpdateQuery, csvOfRun, 1, 196, "delta5", "pwd"))
     return "<h1> Delta 5 is running! </h1>"
 
 @app.route('/caitlin/')
 def caitlinRun():
     global csvOfRun
+    global runnerUpdateQuery = fr.getRunnerUpdateQuerySet
     if csvOfRun == None:
         csvOfRun = open('./static/data/fakeRunnerInEv.csv', 'r').readlines()
-    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "cgjohnson4", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (runnerUpdateQuery, csvOfRun, 1, 196, "cgjohnson4", "pwd"))
     return "<h1> Caitlin is running! </h1>"
 
 @app.route('/frank/')
 def frankRun():
     global csvOfRun
+    global runnerUpdateQuery = fr.getRunnerUpdateQuerySet
     if csvOfRun == None:
         csvOfRun = open('./static/data/fakeRunnerInEv.csv', 'r').readlines()
-    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "frank", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (runnerUpdateQuery, csvOfRun, 1, 196, "frank", "pwd"))
     return "<h1> Frank is running! </h1>"
 
 @app.route('/lisa/')
 def lisaRun():
     global csvOfRun
+    global runnerUpdateQuery = fr.getRunnerUpdateQuerySet
     if csvOfRun == None:
         csvOfRun = open('./static/data/fakeRunnerInEv.csv', 'r').readlines()
-    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "LisaAMarz", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (runnerUpdateQuery, csvOfRun, 1, 196, "LisaAMarz", "pwd"))
     return "<h1> Lisa is running! </h1>"
 
 if __name__ == "__main__":
     ''' This is where stuff goes that will need to run when the server is started
     '''
     global csvOfRun
+    global runnerUpdateQuery = fr.getRunnerUpdateQuerySet
     csvOfRun = open('./static/data/fakeRunnersInEv.csv', 'r').readlines()
     app.debug = True
     app.run()
