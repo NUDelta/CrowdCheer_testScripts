3f8f46da915c45a41d40f0b01cf8a34bc911ced6
moliri <lmaliakal@u.northwestern.edu>
2016-04-15 14:21:14 -0500

    adding short run

--- app/run.py
+++ app/run.py
@@ -26,6 +26,14 @@ def fakeRun():
     thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "1wiATmCxxI", "delta", "pwd"))
     return "<h1> Stella is running! </h1>"
 
+@app.route('/stella_short/')
+def fakeRun():
+    global csvOfRun
+    if csvOfRun == None:
+        csvOfRun = open('./static/data/fakeRunnerInEv_short.csv', 'r').readlines()
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "1wiATmCxxI", "delta", "pwd"))
+    return "<h1> Stella is running! </h1>"
+
 @app.route('/caitlin/')
 def caitlinRun():
     global csvOfRun
