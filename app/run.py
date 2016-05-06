f060c21dd63b09132248dc8335009678b0c57bad
moliri <lmaliakal@u.northwestern.edu>
2016-05-06 14:41:33 -0500

    trying 5 runners at once in scripts

--- app/run.py
+++ app/run.py
@@ -58,6 +58,46 @@ def lisaRun():
     thread.start_new_thread(fr.fakeNewCheerFromCSV, (csvOfRun, 1, 196, "3f0fMYzzEa", "LisaAMarz", "pwd"))
     return "<h1> Lisa is running! </h1>"
 
+
+#STRESS TESTING
+
+@app.route('/stress20/')
+def stressRun():
+    global csvOfRun
+    if csvOfRun == None:
+        csvOfRun = open('./static/data/fakeRunnerInEv.csv', 'r').readlines()
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "alpha", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "efXg4Fgwtz", "bravo", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "QdFwul0E5y", "charlie", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "1wiATmCxxI", "delta", "pwd"))
+    thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "2xPmMmDkwF", "echo", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "foxtrot", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "golf", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "hotel", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "india", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "juliet", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "kilo", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "lima", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "mike", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "november", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "oscar", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "papa", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "quebec", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "romeo", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "sierra", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "tango", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "uniform", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "victor", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "whiskey", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "xray", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "yankee", "pwd"))
+    # thread.start_new_thread(fr.fakeNewRunFromCSV, (csvOfRun, 1, 196, "GoNYnHZroG", "zulu", "pwd"))
+
+
+    return "<h1> Stress Testing: 26 runners are running! </h1>"
+
+
+
 if __name__ == "__main__":
     ''' This is where stuff goes that will need to run when the server is started
     '''
